7da5290c73ff7f9b84d37dd2e08170b4971a3311
wec2t20a20hp3nea024q-4q3-q343-5hq35y-q3 <lichingshun@gmail.com>
2026-03-23 19:33:44 +0800

    Create account.js

---
+++ account.js
@@ -0,0 +1,43 @@
+// account.js
+const users = [
+    { username: "root", password: "114514" },
+    { username: "Jonah", password: "4y27" },
+    { username: "user", password: "abcd" }
+];
+
+function authenticate(username, password) {
+    return users.find(user => user.username === username && user.password === password);
+}
+
+let currentUser = null;
+
+function loadCurrentUser() {
+    const stored = localStorage.getItem('currentUser');
+    if (stored) {
+        currentUser = JSON.parse(stored);
+    } else {
+        currentUser = null;
+    }
+}
+
+function saveCurrentUser(user) {
+    if (user) {
+        localStorage.setItem('currentUser', JSON.stringify(user));
+        currentUser = user;
+    } else {
+        localStorage.removeItem('currentUser');
+        currentUser = null;
+    }
+}
+
+function isLoggedIn() {
+    return currentUser !== null;
+}
+
+function requireAuth() {
+    if (!isLoggedIn()) {
+        window.location.href = 'javascript:history.back()';
+    }
+}
+
+loadCurrentUser();
