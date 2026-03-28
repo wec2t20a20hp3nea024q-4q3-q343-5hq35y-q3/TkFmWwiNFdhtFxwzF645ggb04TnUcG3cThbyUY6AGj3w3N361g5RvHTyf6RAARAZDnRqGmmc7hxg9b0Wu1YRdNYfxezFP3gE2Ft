05ad6238e4e7c5abab02c679f44d08bc419e8ad3
wec2t20a20hp3nea024q-4q3-q343-5hq35y-q3 <lichingshun@gmail.com>
2026-03-28 18:30:12 +0800

    Update timeline.js

--- quiz/timeline.js
+++ quiz/timeline.js
@@ -1,15 +1,6 @@
 // timeline.js
 const timelineEvents = [
-    { name: "F4 2nd UT English",   date: "2026-03-23T08:30:00" },
-    { name: "F4 2nd UT Physics",   date: "2026-03-23T10:00:00" },
-    { name: "F4 2nd UT ICT",       date: "2026-03-23T11:15:00" },
-    { name: "F4 2nd UT Chinese",   date: "2026-03-24T08:30:00" },
-    { name: "F4 2nd UT M2",        date: "2026-03-24T11:15:00" },
-    { name: "F4 2nd UT Math Core", date: "2026-03-25T08:30:00" },
-    { name: "F4 2nd UT Biology",   date: "2026-03-25T10:00:00" },
-    { name: "F4 2nd UT CSD",       date: "2026-03-26T08:30:00" },
-    { name: "F4 2nd UT Chemistry", date: "2026-03-27T08:30:00" },
-    { name: "F4 2nd UT Economics", date: "2026-03-26T10:00:00" },
+    { name: "Resume School", date: "2026-04-09T00:00:01"},
     { name: "F4 Final", date: "2026-06-05T00:00:00" }
     
 ];
